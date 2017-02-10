9dc046a906fd6e29dd24f2bb04636a8bd5c4372d
MSI\ReneZurita <rene.zurita.sanchez@gmail.com>
2017-02-10 14:29:04 +0000

    Spike Tile WiP
    + partially implemented the spike tile functionality

---
+++ Assets/Scripts/Tile/TileSpike.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSpike : Tile {
+	public GameObject spikeGameObject;
+	public float springSize;
+	private Spikes spikes;
+
+	public void Awake(){
+		spikes = spikeGameObject.GetComponent<Spikes> ();
+		type = TileType.Spike;
+	}
+
+	public override void OnStepIn ()
+	{
+		spikes.Spring ();
+	}
+
+}
---
+++ Assets/Scripts/Tile/Traps/Spikes.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour, ISpringTrap {
+	public float springLength = 0.5f;
+	public float springSpeed = 0.5f;
+	private Vector3 originalPosition;
+	private Vector3 sprungPosition;
+
+	public void Awake(){
+		originalPosition = transform.position;
+		sprungPosition = originalPosition + Vector3.up * springLength;
+	}
+
+	#region ISpringTrap implementation
+	public void Spring ()
+	{
+		StartCoroutine (Activate ());
+	}
+	#endregion
+
+	private IEnumerator Activate(){
+		while (true) {
+			transform.position = Vector3.MoveTowards (transform.position, sprungPosition, springSpeed);
+			if (transform.position == sprungPosition)
+				break;
+			yield return null;
+		}
+		while (true) {
+			transform.position = Vector3.MoveTowards (transform.position, originalPosition, springSpeed);
+			if (originalPosition == transform.position)
+				break;
+			yield return null;
+		}
+	}
+}
